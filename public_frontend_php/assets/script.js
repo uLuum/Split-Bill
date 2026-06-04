943d2da46b333bb0f86ffe93b5a0e566208c748b
noeluum <noeloemsalloe2@gmail.com>
2026-06-04 15:23:25 +0700

    Update script.js

--- public_frontend_php/assets/script.js
+++ public_frontend_php/assets/script.js
@@ -1,4 +1,4 @@
-const backendUrl = 'http://split.nurulum.web.id';
+const backendUrl = 'https://split.nurulum.web.id';
 
 /* ===================== Helper ===================== */
 function el(tag, props = {}) {
